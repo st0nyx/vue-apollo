c95689c5fa7eb206fc5fefb00652527822c50e93
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-01-11 10:16:58 +0100

    added graphql mutations to add new user

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    <user-form />
     <user-list />
   </div>
 </template>
 
 <script>
 import UserList from '@/components/UserList.vue'
+import UserForm from "@/components/UserForm";
 
 export default {
   components: {
-    UserList
+    UserList,
+    UserForm
   }
 }
 </script>
